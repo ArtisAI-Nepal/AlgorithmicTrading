fc9df4ce789aa91492168e52f2d9118a59e97f30
shekharkoirala <shekharkoirala4@gmail.com>
2023-04-07 18:45:01 +0100

    updated indicator

---
+++ src/indicators/indicator.py
@@ -0,0 +1,18 @@
+# macd spy data
+import numpy as np
+
+
+class Indicators():
+
+    def __init__(self):
+        pass
+
+    def get_data(self):
+        pass
+
+    def MACD(self):
+        pass
+
+if __name__ == "__main__":
+    ind = Indicators()
+    ind.MACD()
